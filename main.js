2dc7bcc5465094b725359b674a196f8137101a5e
glop35 <gustavolop914@gmail.com>
2026-03-30 22:46:29 -0700

    Limit random color values to a smaller range
    Changed random color generation range from 0-255 to 0-63.

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 document.addEventListener("DOMContentLoaded",() => {
     const button = document.querySelector(".btn");
     button == null || button === void ? void 0 : button.addEventListener("click", () => {
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
+        const r = Math.floor(Math.random() * 64);
+        const g = Math.floor(Math.random() * 64);
+        const b = Math.floor(Math.random() * 64);
         document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
         });
 });
