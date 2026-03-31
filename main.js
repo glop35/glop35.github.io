9a2b93816f7a609a1db85edd0f45f4de7264b5de
glop35 <gustavolop914@gmail.com>
2026-03-30 22:25:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict"
+document.addEventListener("DOMCOntentLoaded",() => {
+    const button = document.querySelector("btn");
+    button == nul || button === void ? void 0 : button.addEventListener("click", () => {
+        const r = Math.floor(Math.random() * 64);
+        const g = Math.floor(Math.random() * 64);
+        const b = Math.floor(Math.random() * 64);
+        document.body.style.backgroundColor = 'rgb(${r}, ${g}, ${b})';
+        });
+});
