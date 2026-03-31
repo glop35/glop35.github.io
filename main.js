d8de2f6dca63121b5dffd78316aec77845c444d3
glop35 <gustavolop914@gmail.com>
2026-03-30 22:44:13 -0700

    Fix color randomization and button selector
    fixed syntax errors

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 "use strict";
-document.addEventListener("DOMCOntentLoaded",() => {
-    const button = document.querySelector("btn");
-    button == nul || button === void ? void 0 : button.addEventListener("click", () => {
-        const r = Math.floor(Math.random() * 64);
-        const g = Math.floor(Math.random() * 64);
-        const b = Math.floor(Math.random() * 64);
-        document.body.style.backgroundColor = 'rgb(${r}, ${g}, ${b})';
+document.addEventListener("DOMContentLoaded",() => {
+    const button = document.querySelector(".btn");
+    button == null || button === void ? void 0 : button.addEventListener("click", () => {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
         });
 });
